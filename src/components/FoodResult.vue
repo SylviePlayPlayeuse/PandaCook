45989addec5066447c7b6c8316401cbdd465b654
Sylvie-godard <sylvie-godard@hotmail.fr>
2024-10-27 10:24:34 +0100

    add component FoodResult and Loader, update initialize data in parent

---
+++ src/components/FoodResult.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type {Food} from "@/helpers/listFood-types";
+
+defineProps<{
+    foodResult: Food[];
+    isLoading: boolean;
+    onInput: (event: Event) => void;
+}>();
+
+</script>
+
+<template>
+    <input type="text" @input="onInput" placeholder="...Filtrer"/>
+    <div v-if="isLoading">
+        is loading ...
+    </div>
+    <ul v-else>
+        <li v-for="food in foodResult" :key="food.name">
+            {{ food.name }} - {{ food.price }}
+        </li>
+    </ul>
+</template>
--- src/components/ListFood.vue
+++ src/components/ListFood.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import {computed, onMounted, ref} from "vue";
+import {computed, ref} from "vue";
 import {_debounce} from "@/tools/Tools";
 import {type Food, NO_RESULTS_MESSAGE} from "@/helpers/listFood-types";
-import {getInitialFood} from "@/api/food.api";
+import FoodResult from "@/components/FoodResult.vue";
 
-const listFood = ref<Food[]>([]);
-const foodResult = ref<Food[]>([]);
+const props = defineProps<{
+    initialList: Food[],
+}>();
+
 const isLoading = ref<boolean>(false);
-
-const initializeFood = (newListFood: Food[]) => {
-    listFood.value = newListFood;
-    foodResult.value = newListFood;
-}
+const listFood = ref<Food[]>(props.initialList);
+const foodResult = ref<Food[]>(props.initialList);
 
 const debouncedSearch = _debounce((term: string) => {
     foodResult.value = listFood.value.filter((food) => {
@@ -21,20 +20,15 @@ const debouncedSearch = _debounce((term: string) => {
 
 }, 300);
 
-const noResultsMessage = computed(() => {
+const noResultsMessage = computed<string>(() => {
     return foodResult.value.length === 0 ? NO_RESULTS_MESSAGE : ''
 })
 
-const handleInput = (event: Event) => {
+const handleInput = (event: Event): void => {
     isLoading.value = true;
     const target = event.target as HTMLInputElement;
     debouncedSearch(target.value);
 }
-
-onMounted(async () => {
-    const initialFood: Food[] = await getInitialFood();
-    initializeFood(initialFood)
-})
 
 </script>
 
@@ -45,15 +39,11 @@ onMounted(async () => {
                 {{ food.name }} - {{ food.price }}
             </li>
         </ul>
-        <input type="text" @input="handleInput" placeholder="...Filtrer"/>
-        <div v-if="isLoading">
-            is loading ...
-        </div>
-        <ul v-else>
-            <li v-for="food in foodResult" :key="food.name">
-                {{ food.name }} - {{ food.price }}
-            </li>
-        </ul>
+        <FoodResult
+            :foodResult="foodResult"
+            :isLoading="isLoading"
+            :onInput="handleInput"
+        />
         <p>
             {{ noResultsMessage }}
         </p>
